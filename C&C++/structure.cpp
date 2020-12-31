68d0dbbd6e523464f46f6b5518c248079d100a36
devblin <110119035@nitt.edu>
2020-12-31 12:16:03 +0530

    updated readme

--- C&C++/structure.cpp
+++ C&C++/structure.cpp
@@ -48,6 +48,15 @@ struct __attribute__((__packed__)) Student2
 	//total size = 4+25+50 = 79
 };
 
+struct Card
+{
+	int face;  //1,2,3....,10,(J = 11), (Q = 12), (K = 13)
+	int shape; //spade = 0, club = 1, heart = 2, diamond = 3
+	int color; //red = 0, black = 1
+
+	//total size = (int) * 3 = 4*3 = 12
+};
+
 int main()
 {
 	struct Rectangle r;			  //declaring r as Rectangle struct
@@ -64,6 +73,16 @@ int main()
 
 	struct Student2 s1;
 	cout << "Size of s (without padding): " << sizeof(s1) << "\n"; //we will get 79, bcoz we avoid the padding of memory
+
+	struct Card c = {1, 1, 1};
+	cout << "Size of c: " << sizeof(c) << "\n";
+	cout << "Face: " << c.face << "\nColor: " << c.color << "\nShape: " << c.shape << "\n";
+
+	struct Student s2[2] = {{1, "Deep", "Apne ghar mei"}};
+	cout << "----------\nStudent 1:\n----------\n";
+	cout << "Roll No.: " << s2[0].roll << "\nName: " << s2[0].name << "\nAddress: " << s2[0].address << '\n';
+	cout << "----------\nStudent 2:\n----------\n";
+	cout << "Roll No.: " << s2[1].roll << "\nName: " << s2[1].name << "\nAddress: " << s2[1].address << '\n';
 	return 0;
 }
 
