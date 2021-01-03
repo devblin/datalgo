1d1638ae12f99257101eedba4c1a841bf01ffb37
devblin <110119035@nitt.edu>
2021-01-03 11:53:00 +0530

    add function

---
+++ C&C++/functions.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int add(int x, int y)
+{
+	return (x + y);
+}
+
+int main()
+{
+	/*
+	FUNCTIONS:
+	Monolithic Programming -> Everthing inside main.
+	Modular/Procedural Programming -> Program consisting of several functions.
+	*/
+	int x, y, z;
+	x = 10;
+	y = 5;
+	z = add(x, y);
+	cout << "Sum x & y: " << z << "\n";
+
+	return 0;
+}
+
+/*
+MAIN MEMORY
+----------------------
+HEAP
+----------------------
+
+
+----------------------
+STACK
+----------------------
+
+
+----------------------
+CODE SECTION
+----------------------
+
+
+----------------------
+*/
