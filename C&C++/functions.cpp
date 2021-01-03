e096999cb03bb5cbc3450560b87007b87e60759b
devblin <110119035@nitt.edu>
2021-01-03 11:59:40 +0530

    updated add function

--- C&C++/functions.cpp
+++ C&C++/functions.cpp
@@ -1,22 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int add(int x, int y)
+/*
+FUNCTIONS:
+Monolithic Programming -> Everthing inside main.
+Modular/Procedural Programming -> Program consisting of several functions.
+*/
+
+int add(int a, int b) //prototype of function, a, b are called formal parameters
 {
-	return (x + y);
+	return (a + b);
 }
 
 int main()
 {
-	/*
-	FUNCTIONS:
-	Monolithic Programming -> Everthing inside main.
-	Modular/Procedural Programming -> Program consisting of several functions.
-	*/
 	int x, y, z;
 	x = 10;
 	y = 5;
-	z = add(x, y);
+	z = add(x, y); //function call, where x, y are called actual parameters
 	cout << "Sum x & y: " << z << "\n";
 
 	return 0;
@@ -32,12 +33,13 @@ HEAP
 ----------------------
 STACK
 ----------------------
-
-
+add: a[10] b[5]  (gets deleted after return)
+main: x[10] y[5] z[15] (prints 15)
 ----------------------
 CODE SECTION
 ----------------------
-
+add()
+main()
 
 ----------------------
 */
