fb1aacadf6b2ba398ab7ec2c16c4342496f33e21
devblin <deepanshu.dhruw@outlook.com>
2021-01-06 09:11:04 +0530

    updated struct parameter

--- C&C++/struct_parameter.cpp
+++ C&C++/struct_parameter.cpp
@@ -9,6 +9,7 @@ struct Rectangle
 
 int Area(struct Rectangle r)
 {
+	cout << "Area: ";
 	return r.width * r.length;
 }
 
@@ -16,7 +17,20 @@ int AreaRef(struct Rectangle &r)
 {
 	r.width++;
 	r.length++;
+	cout << "AreaRef: ";
 	return r.length * r.width;
+}
+
+void ChangeLength(struct Rectangle *r, int x)
+{
+	cout << "Changing length to " << x << "\n";
+	r->length = x;
+}
+
+void ChangeWidth(struct Rectangle *r, int x)
+{
+	cout << "Changing width to " << x << "\n";
+	r->width = x;
 }
 
 int main()
@@ -27,4 +41,8 @@ int main()
 	cout << "Incrementing length & width\n";
 	cout << AreaRef(r) << "\n";
 	cout << "r.length: " << r.length << "\nr.width: " << r.width << "\n";
+	ChangeLength(&r, 12);
+	cout << Area(r) << "\n";
+	ChangeWidth(&r, 12);
+	cout << Area(r) << "\n";
 }
