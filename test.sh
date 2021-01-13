e70a9b5653ea28344a8145e020c4bd1a16c3809f
devblin <deepanshu.dhruw@outlook.com>
2021-01-13 16:11:03 +0530

    updated script

--- test.sh
+++ test.sh
@@ -6,26 +6,35 @@ YELLOW='\033[1;33m'
 GREEN='\033[0;32m'
 LGREEN='\033[1;32m'
 
-printf "${CYAN}-------------DIR Name:--------------${NC}\n"
-printf "${YELLOW}0. practice\n1. C&C++${NC}\n"
-printf "${CYAN}Enter the SI.No.:${NC} "
-read OPTION
-
 execute() {
-	printf "\n${GREEN}Available Files:${NC}\n"
+	printf "\n${GREEN}>> Available Files:${NC}\n"
 	printf "${YELLOW}"
 	ls "${1}"
-	printf "${NC}${CYAN}\nEnter Filename: ${NC}"
+	printf "${NC}${CYAN}\n>> Enter Filename: ${NC}"
 	read FILENAME
-	printf "${LGREEN}Please Wait...${NC}\n"
+	printf "${YELLOW}Please Wait...${NC}\n"
 	g++ -o test "${1}/${FILENAME}.cpp"
+	printf "${LGREEN}Code Complied...\n${NC}"
 	./test
+	printf "\n${CYAN}---------------------------------------------${NC}"
 }
 
-if [ $OPTION -eq 0 ]
-then
-	execute 'practice'
-elif [ $OPTION -eq 1 ]
-then
-	execute 'C&C++'
-fi
+while true;
+do
+	printf "\n${CYAN}---------RUN & COMPILE C++ PROGRAMS----------${NC}\n"
+	printf "${YELLOW}1. practice\n2. C&C++\n${NC}${RED}0. Exit${NC}\n"
+	printf "${CYAN}>> Enter the SI.No.:${NC} "
+	read OPTION
+
+	if [ $OPTION -eq 1 ]
+	then
+		execute 'practice'
+	elif [ $OPTION -eq 2 ]
+	then
+		execute 'C&C++'
+	elif [ $OPTION -eq 0 ]
+	then
+		printf "${RED}\nBye-Bye${NC}\n"
+		break
+	fi
+done
