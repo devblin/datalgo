6893bb64c4a2cfeeabb9ef9236e2a212da610bf2
devblin <deepanshu.dhruw@outlook.com>
2021-01-18 21:14:39 +0530

    added modular

---
+++ C&C++/modular.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void area(int l, int b)
+{
+	cout << "Area of rectangle: " << l * b << "\n";
+}
+
+void perimeter(int l, int b)
+{
+	cout << "Perimeter of rectangle: " << 2 * (l + b) << "\n";
+}
+
+int main()
+{
+	int l = 0, b = 0;
+	area(l, b);
+	perimeter(l, b);
+	cout << "Enter length & width: ";
+	cin >> l >> b;
+	area(l, b);
+	perimeter(l, b);
+	return 0;
+}
