0ef4e5226d3e6928a2929c0e0479650b5688ffaa
devblin <deepanshu.dhruw@outlook.com>
2021-03-05 13:05:11 +0530

    updated recursion/tree

--- recursion/tree.cpp
+++ recursion/tree.cpp
@@ -1,2 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+void fun(int n)
+{
+	if (n > 0)
+	{
+		cout << n << " ";
+		fun(n - 1);
+		fun(n - 1);
+	}
+}
+
+void fun1(int n)
+{
+	if (n > 0)
+	{
+		fun1(n - 1);
+		cout << n << " ";
+		fun1(n - 1);
+	}
+}
+
+int main()
+{
+	int x;
+	cin >> x;
+	fun(x);
+	cout << "\n";
+	fun1(x);
+	return 0;
+}
