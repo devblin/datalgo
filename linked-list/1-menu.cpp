3afaf6f61070578962699cbfdbaffbb19ad08ac7
devblin <dhruwxl@gmail.com>
2021-06-27 23:04:52 +0530

    added upto node delete

--- linked-list/1-menu.cpp
+++ linked-list/1-menu.cpp
@@ -11,7 +11,7 @@ public:
 class LinkedList
 {
 public:
-    Node *first;
+    Node *first, *last;
 
     LinkedList() { first = NULL; }
     LinkedList(int a[], int n);
@@ -30,11 +30,16 @@ public:
     Node *LSearchR(int key);
     Node *LSearchMTH(int key);
     Node *LSearchT(int key);
+    void Insert(int pos, int val);
+    void Append(int val);
+    void InsertSorted(int val);
+    void Delete(int pos);
+    bool CheckSorted();
 };
 
 LinkedList::LinkedList(int a[], int n)
 {
-    Node *last, *temp;
+    Node *temp;
     int i = 0;
     first = new Node;
     first->data = a[0];
@@ -241,17 +246,145 @@ Node *LinkedList::LSearchT(int key)
     return ans;
 }
 
+void LinkedList::Insert(int pos, int val)
+{
+    Node *newNode, *p;
+    newNode = new Node;
+    newNode->data = val;
+    p = first;
+    if (pos < 0 || pos > NodeCount())
+        cout << "Invalid pos\n";
+    else
+    {
+        if (first == NULL)
+            first = last = newNode;
+        else
+        {
+            if (pos == NodeCount())
+                last = newNode;
+            if (pos == 0)
+            {
+                newNode->next = first;
+                first = newNode;
+            }
+            else
+            {
+                pos = pos - 1;
+                while (pos--)
+                    p = p->next;
+                newNode->next = p->next;
+                p->next = newNode;
+            }
+        }
+    }
+    DisplayI();
+}
+
+void LinkedList::Append(int val)
+{
+    Node *newNode;
+    newNode = new Node;
+    newNode->data = val;
+    if (first == NULL)
+        first = last = newNode;
+    else
+    {
+        last->next = newNode;
+        last = newNode;
+    }
+    DisplayI();
+}
+
+void LinkedList::InsertSorted(int val)
+{
+    Node *newNode, *p, *temp;
+    newNode = new Node;
+    newNode->data = val;
+    p = first;
+    if (!first)
+        first = last = newNode;
+    else if (first->data > val)
+    {
+        newNode->next = first;
+        first = newNode;
+    }
+    else
+    {
+        while (p->next != NULL && p->next->data < val)
+            p = p->next;
+        if (p->next == NULL)
+            last->next = newNode;
+        else
+        {
+            temp = p->next;
+            p->next = newNode;
+            newNode->next = temp;
+        }
+    }
+    DisplayI();
+}
+
+void LinkedList::Delete(int pos)
+{
+    if (first == NULL)
+    {
+        cout << "Linked list empty\n";
+        return;
+    }
+    Node *p, *temp;
+    p = first;
+    pos = pos - 1;
+    if (pos == 0)
+    {
+        temp = first;
+        first = first->next;
+        delete temp;
+    }
+    else if (pos >= 0 && pos <= NodeCount())
+    {
+        while (--pos)
+        {
+            p = p->next;
+        }
+        temp = p->next;
+        p->next = p->next->next;
+        delete temp;
+    }
+    else
+        cout << "Invalid pos\n";
+    DisplayI();
+}
+
+bool LinkedList::CheckSorted()
+{
+    Node *temp;
+    temp = new Node;
+    temp = first;
+    while (temp->next != NULL)
+    {
+        if (temp->data > temp->next->data)
+            return false;
+        temp = temp->next;
+    }
+    return true;
+}
+
 void menu(LinkedList *link)
 {
     Node *temp;
     temp = link->first;
-    int option, i, n, key;
+    int option, i, n, key, pos;
     cout << "-----LINKED-LIST-----\n";
     cout << "1. Display\n";
     cout << "2. CountNode\n";
     cout << "3. Sum\n";
     cout << "4. Max\n";
     cout << "5. Search\n";
+    cout << "6. Insert\n";
+    cout << "7. Append\n";
+    cout << "8. InsertSorted\n";
+    cout << "9. Delete\n";
+    cout << "10. CheckSorted\n";
     cout << "Enter option: ";
     cin >> option;
     int a[n];
@@ -290,6 +423,29 @@ void menu(LinkedList *link)
         link->first = temp;
         cout << "LinearSearch(MTH): " << link->LSearchMTH(key) << "\n";
         cout << "LinearSearch(T): " << link->LSearchT(key) << "\n";
+        break;
+    case 6:
+        cout << "Enter pos & value: ";
+        cin >> pos >> key;
+        link->Insert(pos, key);
+        break;
+    case 7:
+        cout << "Enter value: ";
+        cin >> key;
+        link->Append(key);
+        break;
+    case 8:
+        cout << "Enter value: ";
+        cin >> key;
+        link->InsertSorted(key);
+        break;
+    case 9:
+        cout << "Enter pos: ";
+        cin >> pos;
+        link->Delete(pos);
+        break;
+    case 10:
+        cout << "Is sorted: " << link->CheckSorted() << "\n";
         break;
     default:
         break;
